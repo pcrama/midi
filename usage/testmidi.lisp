928f27bf40e4509166788703dd15f3424250a009
Philippe Crama <pcfeb0009@gmx.com>
2011-11-19 01:48:52 +0100

    Prepare basic drum tracks as metronome

--- usage/testmidi.lisp
+++ usage/testmidi.lisp
@@ -104,3 +104,68 @@ http://chordfind.com/
 	(slot-value (first timing-track) 'bb) 8)
   (defparameter *timing* timing-track)
   (write-midi-file midi-file "/tmp/t.mid"))
+
+(defun make-metronome-track (name beats/measure beats/min duration)
+  "Create a Midi file NAME usable as a metronome: BEATS/MEASURE, BEATS/MIN and
+DURATION \(in seconds\)"
+  #|
+  (loop for speed from 140 to 188 by 8 do
+           (make-metronome-track (format nil "/tmp/valse~A.mid" speed)
+                                 3
+                                 speed
+                                 (* 60 10)))
+NIL
+  (loop for speed from 60 to 140 by 4 do
+           (make-metronome-track (format nil "/tmp/c~A.mid" speed)
+                                 4
+                                 speed
+                                 (* 60 10)))
+  |#
+  (let* ((+acoustic-bass-drum+ 35)
+         (+low-mid-tom+ 47)
+         (measure-count (ceiling (/ duration 60)
+                                 (/ beats/measure beats/min)))
+         (ticks/beat 60)
+         (*midi-channel* 9)
+         (timing-track
+          (list (make-instance 'time-signature-message
+                               :time 0)
+                (make-instance 'tempo-message
+                               :time 0
+                               :tempo (round 60000000 beats/min))))
+         (drum-track
+          (flet ((make-sound (key vel time duration)
+                   (list (make-instance 'note-on-message
+                                        :time time
+                                        :key key
+                                        :velocity vel)
+                         (make-instance 'note-off-message
+                                        :time (+ time duration)
+                                        :key key
+                                        :velocity vel))))
+            (cons (make-instance 'program-change-message
+                                 :program 0
+                                 :time 0)
+                  (loop with now = (- ticks/beat)
+                     with duration = (/ ticks/beat 5)
+                     repeat measure-count nconc
+                       (nconc (make-sound +low-mid-tom+
+                                          127
+                                          (incf now ticks/beat)
+                                          (round (* 1.5 duration)))
+                              (when (> beats/measure 1)
+                                (loop repeat (1- beats/measure) nconc
+                                     (make-sound +acoustic-bass-drum+
+                                                 80
+                                                 (incf now ticks/beat)
+                                                 duration))))))))
+         (midi-file
+          (make-instance 'midifile
+                         :format 1
+                         :division ticks/beat
+                         :tracks (list timing-track drum-track))))
+    (setf (slot-value (first timing-track) 'dd) 2
+          (slot-value (first timing-track) 'nn) 4
+          (slot-value (first timing-track) 'cc) 24
+          (slot-value (first timing-track) 'bb) 8)
+    (write-midi-file midi-file name)))
